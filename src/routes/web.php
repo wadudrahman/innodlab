d11e902aa47a01a81d7a45b60e179c873ae1bf4e
abirahsan007 <mahsan172077@bscse.uiu.ac.bd>
2021-02-18 04:18:54 +0600

    WIP dashboard UI

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,3 +26,7 @@ Route::post('/password/reset', [AuthController::class, 'resetPassword']);
 Route::get('/packages', [AuthController::class, 'showPackagesView']);
 Route::get('/register', [AuthController::class, 'showRegistrationOptionsView']);
 Route::get('/register/{userRoleId}', [AuthController::class, 'showPackagesView']);
+//panel routes
+Route::get('/panel/dashboard', [DashboardController::class, 'showDashboardView']);
+Route::get('/panel/users', [DashboardController::class, 'showUsersView']);
+Route::get('/panel/chats', [DashboardController::class, 'showChatsView']);
