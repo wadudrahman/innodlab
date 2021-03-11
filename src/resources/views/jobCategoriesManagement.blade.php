ab9ef2167eb65b582595fc375c825d494aff0a23
abirahsan007 <mahsan172077@bscse.uiu.ac.bd>
2021-03-11 21:55:21 +0600

    WIP job category UI(3)

--- src/resources/views/jobCategoriesManagement.blade.php
+++ src/resources/views/jobCategoriesManagement.blade.php
@@ -67,7 +67,14 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-6 col-md-12 mb-4">
+
+                {{--<div class="col-lg-6 col-md-12 mb-4">
+
+                </div>--}}
+            </div>
+
+            <div class="row">
+                <div class="col-12">
                     <div class="card">
                         <div class="card-body">
                             <h5 class="card-title">Enlisted Job Categories</h5>
