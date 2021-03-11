73505ad7e485a052ef40ef610075a22effd22acc
abirahsan007 <mahsan172077@bscse.uiu.ac.bd>
2021-03-12 02:31:02 +0600

    user management(3)

--- src/resources/views/userManagement.blade.php
+++ src/resources/views/userManagement.blade.php
@@ -48,8 +48,9 @@
                                     <th scope="col">#</th>
                                     <th scope="col">Name</th>
                                     <th scope="col">Username</th>
-                                    <th scope="col">Action</th>
                                     <th scope="col">Role</th>
+                                    <th scope="col">Action</th>
+
                                 </tr>
                                 </thead>
                                 <tbody>
